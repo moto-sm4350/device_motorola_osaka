65022ed57814fb042644a68d1a53ad8b92efbe1c
Dan Morrill <morrildl@google.com>
2010-03-05 16:16:19 -0800

    osaka: Add standard feature flag indicating device supports WiFi.
    Change-Id: I6c30e5fed791db99b4d2382d74027902ab360675

--- device.mk
+++ device.mk
@@ -259,6 +259,7 @@ $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.
 
 # WiFi
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 PRODUCT_PACKAGES += \

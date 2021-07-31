62d26a29c151221899afd58a1ea8d8f5261318a7
Alexander Koskovich <zvnexus@outlook.com>
2021-07-30 22:56:55 -0700

    osaka: Initial product definition for Motorola Moto G Stylus 2021 (5G).
    Change-Id: I2886be56cc6d6b2a22eabf8c91615f3c7349f2d1
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The Raphielscape LLC. and The helluvaOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/hentai_osaka.mk
+
+COMMON_LUNCH_CHOICES := \
+    hentai_osaka-eng \
+    hentai_osaka-user \
+    hentai_osaka-userdebug
---
+++ BoardConfig.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2021 The Raphielscape LLC. and The helluvaOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include build/make/target/board/BoardConfigMainlineCommon.mk
+
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    product \
+    system \
+    system_ext \
+    vbmeta_system \
+    vendor_boot
+
+# AVB
+BOARD_AVB_VBMETA_SYSTEM := product system system_ext vendor
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76
+
+# Kernel
+TARGET_NO_KERNEL := true
+
+# Partitions
+BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := product system system_ext vendor
+BOARD_MOTOROLA_DYNAMIC_PARTITIONS_SIZE := 6979317760 # (BOARD_SUPER_PARTITION_SIZE / 2) - 4096
+BOARD_SUPER_PARTITION_GROUPS := motorola_dynamic_partitions
+BOARD_SUPER_PARTITION_SIZE := 13958643712
+
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4

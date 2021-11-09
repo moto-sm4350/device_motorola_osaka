bcaa9c64083919c40925e0b3ec3a20726707dea7
electimon <electimon@gmail.com>
2021-11-09 03:44:12 +0000

    osaka: Build extra interfaces
    * Caused by switching over to LOS-Modded
    
    Signed-off-by: electimon <electimon@gmail.com>

--- device-interfaces.mk
+++ device-interfaces.mk
@@ -1,4 +1,5 @@
 PRODUCT_PACKAGES := \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
@@ -22,5 +23,8 @@ PRODUCT_PACKAGES := \
     android.hardware.secure_element@1.1.vendor \
     android.hardware.secure_element@1.2.vendor \
     android.system.net.netd@1.1.vendor \
-    vendor.qti.hardware.display.mapper@1.1.vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.servicetracker@1.2.vendor
 

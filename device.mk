824eee34df8f8afaee11380b6a987f3fdca362e9
electimon <electimon@gmail.com>
2021-11-09 03:45:10 +0000

    osaka: Build our GNSS HAL
    Signed-off-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,15 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti \
+    libbatching \
+    libgeofencing \
+    libgps.utils \
+    libgnss \
+    libloc_core
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 

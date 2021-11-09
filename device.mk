6cef6a0f2f7f6c9f5f628d2721e88c9f2b48f4cd
electimon <electimon@gmail.com>
2021-11-09 15:46:39 +0000

    osaka: Build out of tree MotoActions
    Signed-off-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -194,6 +194,10 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx.vendor \
     libstagefrighthw
 
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
